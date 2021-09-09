b2342ced935ef4017e0dcfcc21593ce35bccb8b9
Dmitriy Pinchuk <diamondpinchuk@gmail.com>
2021-09-09 13:08:03 +0300

    lab 1a is ready

---
+++ Programming on the Internet/WebApplication1a/PDVGetHttpHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1a
+{
+    public class PDVGetHttpHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string result = "<p>GET-Http-PDV:ParmA = "
+                + context.Request.Params.Get("ParmA")
+                + ", ParmB = " + context.Request.Params.Get("ParmB")
+                + "</p>";
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(result);
+        }
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
