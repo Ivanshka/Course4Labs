48f4de61db9aa15b5075255762ef41e7f61059f9
Dmitriy Pinchuk <diamondpinchuk@gmail.com>
2021-09-10 09:14:02 +0300

    fix 1a lab (add win-form)

---
+++ Programming on the Internet/WebApplication1a_Form/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WebApplication1a_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.x = new System.Windows.Forms.TextBox();
+            this.y = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(274, 12);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(94, 29);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(12, 12);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(125, 27);
+            this.x.TabIndex = 1;
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(143, 12);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(125, 27);
+            this.y.TabIndex = 2;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(12, 49);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(355, 27);
+            this.result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 88);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox result;
+    }
+}
+
---
+++ Programming on the Internet/WebApplication1b/static/WebSocket.js
@@ -0,0 +1,43 @@
+﻿var ta;
+var ws = null;
+var bstart;
+var bstop;
+var supportsWebSockets = 'WebSocket' in window || 'MozWebSocket' in window;
+
+
+window.onload = function () {
+    if (supportsWebSockets) {
+        WriteMessage('support', 'Yes');
+        ta = document.getElementById('ta');
+        bstart = document.getElementById('bstart');
+        bstop = document.getElementById('bstop');
+        bstart.disabled = false;
+        bstop.disabled = true;
+    } else {
+        WriteMessage('support', 'No');
+    }
+}
+
+function WriteMessage(idspan, txt) {
+    var span = document.getElementById(idspan);
+    span.innerHTML = txt;
+}
+
+function exe_start() {
+    if (ws == null) {
+        ws = new WebSocket("ws://localhost:51237/Websockets.websocket");
+        ws.onopen = () => ws.send('Connection')
+        ws.onclose = (s) => console.log('onclose', s);
+        ws.onmessage = (e) => ta.innerHTML += '\n' + e.data;
+        bstart.disabled = true;
+        bstop.disabled = false;
+    }
+}
+
+function exe_stop() {
+    //ws.send("stop");
+    ws.close(3001);
+    ws = null;
+    bstart.disabled = false;
+    bstop.disabled = true;
+}
