4ad7eafd4a7bee021b90e3fa81eac3a2dc8f92c4
Dmitriy Pinchuk <diamondpinchuk@gmail.com>
2021-09-10 09:14:02 +0300

    fix -2a lab (add win-form)

---
+++ Programming on the Internet/WebApplication1a_Form/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebApplication1a_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            string uri = "https://localhost:44385/sum";
+
+            var values = new Dictionary<string, string>
+            {
+                { "X", x.Text },
+                { "Y", y.Text }
+            };
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var content = new FormUrlEncodedContent(values);
+                    var response = await client.PostAsync(uri, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    result.Text = responseString;
+                }
+                catch (Exception exception)
+                {
+                    result.Text = "try again: " + exception.Message;
+                }
+            }
+        }
+    }
+}
---
+++ Programming on the Internet/WebApplication1b/WebSocketHandler.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace WebApplication1b
+{
+    public class WebSocketHandler : IHttpHandler
+    {
+        WebSocket socket;
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketRequest);
+            }
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context)
+        {
+            int i = 0;
+            socket = context.WebSocket;
+            string s = await Receive();
+            await Send(s);
+
+            Thread myThread = new Thread(new ThreadStart(listenToStopCommand));
+            myThread.Start();
+
+            Debug.WriteLine("websocket has started");
+            try
+            {
+                while (socket.State == WebSocketState.Open)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    DateTime time = DateTime.Now;
+                    await Send("[" + time + "]");
+                    Debug.WriteLine(time + " was sent");
+                }
+            }
+            catch (WebSocketException e)
+            {
+                Debug.WriteLine("websocket has been stopped inccorectly");
+                _ = socket.CloseAsync(WebSocketCloseStatus.EndpointUnavailable,
+                    "websocket has been closed incorrectly", CancellationToken.None);
+            }
+        }
+
+        private async Task<string> Receive()
+        {
+            string rc = null;
+            var buffer = new ArraySegment<byte>(new byte[512]);
+            var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+            rc = System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+            return rc;
+        }
+
+        private async Task Send(string s)
+        {
+            var sendbuffer = new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes("Ответ: " + s));
+            await socket.SendAsync(sendbuffer, System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        private async void listenToStopCommand()
+        {
+            string rc = null;
+
+            do
+            {
+                var buffer = new ArraySegment<byte>(new byte[512]);
+
+                Debug.WriteLine("[LISTENER] listening to command ...");
+
+                var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                rc = System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+
+                Debug.WriteLine("[LISTENER] " + rc + " command was received");
+
+                if (rc.Equals("stop"))
+                {
+                    Debug.WriteLine("[LISTENER] websocket has been stopped");
+                    _ = socket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                        "websocket has been closed", CancellationToken.None);
+                    return;
+                }
+                else
+                {
+                    Debug.WriteLine("[LISTENER] unknown command " + rc);
+                }
+
+            } while (rc == null || rc.Equals("stop"));
+        }
+    }
+}
