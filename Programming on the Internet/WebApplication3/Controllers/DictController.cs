9880ea3ee0d54e2c156d5d16c545405b1eefddeb
Dmitriy Pinchuk <diamondpinchuk@gmail.com>
2021-10-04 17:04:36 +0300

    POI_lab_3 is ready

---
+++ Programming on the Internet/WebApplication3/Controllers/DictController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class DictController : Controller
+    {
+        static ContactsHolder holder = new ContactsHolder();
+
+        public ActionResult Index()
+        {
+            ViewBag.allContacts = holder.GetAllContacts();
+
+            return View("Index");
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult AddSave(String lastname, String phoneNumber)
+        {
+            String method = this.HttpContext.Request.HttpMethod;
+
+            if (method.Equals("POST"))
+            {
+                Contact contact = new Contact(lastname, phoneNumber);
+
+                holder.Insert(contact);
+            }
+
+            return Index();
+        }
+
+        public ActionResult Update(String id)
+        {
+            Contact contact = holder.Find(id);
+
+            if (contact != null)
+            {
+                ViewBag.contact = contact;
+                return View();
+            }
+
+            return View();
+        }
+
+        public ActionResult UpdateSave(String id, String lastname, String phoneNumber)
+        {
+            Contact contact = new Contact(id, lastname, phoneNumber);
+
+            holder.Update(contact);
+
+            return Index();
+        }
+
+        public ActionResult Delete()
+        {
+            ViewBag.allContacts = holder.GetAllContacts();
+
+            return View();
+        }
+
+        public ActionResult DeleteSave(String id)
+        {
+            Contact contact = holder.Find(id);
+
+            if (contact != null)
+            {
+                holder.Delete(contact);
+            }
+
+            return Index();
+        }
+    }
+}
---
+++ Programming on the Internet/WebApplication3/Models/ContactsHolder.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebApplication3.Models
+{
+    public class ContactsHolder
+    {
+        private const String jsonFile = "D:/contacts.json";
+
+        [JsonInclude]
+        public List<Contact> JsonDatabase { get; set; } = null;
+        public ContactsHolder()
+        {
+            if (File.Exists(jsonFile))
+            {
+                JsonDatabase = ReadAllContactsFromJsonFile(jsonFile);
+            }
+            else
+            {
+                JsonDatabase = new List<Contact>();
+                WriteAllContactsInJsonFile(jsonFile);
+            }
+        }
+
+        public Contact Find(String id)
+        {
+            return JsonDatabase.FirstOrDefault(contact => contact.Id.Equals(id));
+        }
+
+        public Contact Insert(Contact contact)
+        {
+            contact.Id = Guid.NewGuid().ToString();
+
+            JsonDatabase.Add(contact);
+
+            WriteAllContactsInJsonFile(jsonFile);
+
+            return contact;
+        }
+
+        public Contact Update(Contact contact)
+        {
+            Contact oldContact = Find(contact.Id);
+
+            if (oldContact == null)
+            {
+                return null;
+            }
+
+            JsonDatabase.Remove(oldContact);
+
+            oldContact.Lastname = contact.Lastname;
+            oldContact.PhoneNumber = contact.PhoneNumber;
+
+            JsonDatabase.Add(contact);
+
+            WriteAllContactsInJsonFile(jsonFile);
+
+            return oldContact;
+        }
+
+        public Contact Delete(Contact contact)
+        {
+            Contact oldContact = Find(contact.Id);
+
+            if (oldContact == null)
+            {
+                return null;
+            }
+
+            JsonDatabase.Remove(oldContact);
+
+            WriteAllContactsInJsonFile(jsonFile);
+
+            return oldContact;
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            return JsonDatabase.ToArray();
+        }
+
+        private void DeleteJsonFile(String jsonFile)
+        {
+            if (File.Exists(jsonFile))
+            {
+                File.Delete(jsonFile);
+            }
+
+        }
+
+        private void WriteAllContactsInJsonFile(String jsonFile)
+        {
+            String json = JsonSerializer.Serialize<List<Contact>>(JsonDatabase);
+            File.WriteAllText(jsonFile, json);
+        }
+
+        private List<Contact> ReadAllContactsFromJsonFile(String jsonFile)
+        {
+            String json = File.ReadAllText(jsonFile);
+            return JsonSerializer.Deserialize<List<Contact>>(json);
+        }
+    }
+}
