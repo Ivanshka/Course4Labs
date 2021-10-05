1734c9b70afc19172db84c0f072c44c2f4952b0d
Dmitriy Pinchuk <diamondpinchuk@gmail.com>
2021-10-05 17:14:24 +0300

    POI_lab_4 is ready

---
+++ Programming on the Internet/WebApplication4/ContactContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication4.Models;
+
+namespace WebApplication4
+{
+    public class ContactContext : DbContext
+    {
+
+        public static ContactContext Db { get; private set; } = new ContactContext();
+
+        private ContactContext() : base("testDB")
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Programming on the Internet/WebApplication4/Models/Contact.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Models
+{
+    public class Contact : IComparable<Contact>
+    {
+        [JsonInclude]
+        public String Id { get; set; }
+        [JsonInclude]
+        public String Lastname { get; set; }
+        [JsonInclude]
+        public String PhoneNumber { get; set; }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(String lastname, String phonenumber)
+        {
+            Lastname = lastname;
+            PhoneNumber = phonenumber;
+        }
+
+        public Contact(String id, String lastname, String phonenumber) : this(lastname, phonenumber)
+        {
+            Id = id;
+
+        }
+        public Contact(Contact contact)
+        {
+            if (contact == null)
+            {
+                throw new NullReferenceException();
+            }
+            this.Id = contact.Id;
+            this.Lastname = contact.Lastname;
+            this.PhoneNumber = contact.PhoneNumber;
+        }
+
+        public int CompareTo(Contact contact)
+        {
+            return Lastname.CompareTo(contact.Lastname);
+        }
+    }
+}
