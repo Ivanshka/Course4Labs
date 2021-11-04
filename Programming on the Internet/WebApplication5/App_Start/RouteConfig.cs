fae8468d9c2c08c7e0b25f66a0965c3f47a8b7ad
Dmitriy Pinchuk <diamondpinchuk@gmail.com>
2021-11-04 16:54:40 +0300

    PVI_lab5a is ready

---
+++ Programming on the Internet/WebApplication5/App_Start/RouteConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication5
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "C01",
+                url: "CResearch/{action}",
+                defaults: new { controller = "CResearch", action = "C01" }
+            );
+
+            routes.MapRoute(
+                name: "M03b",
+                url: "V3/{controller}/X/{action}/{id}",
+                defaults: new { controller = "MResearch", action = "M03", id = UrlParameter.Optional }
+            );
+
+            //routes.MapRoute(
+            //    name: "M03",
+            //    url: "V3/{controller}/{action}/{id}",
+            //    defaults: new { controller = "MResearch", action = "M03", id = UrlParameter.Optional }
+            //);
+
+
+            routes.MapRoute(
+                name: "M03",
+                url: "V3/",
+                defaults: new { controller = "MResearch", action = "M03" }
+            );
+
+            routes.MapRoute(
+                name: "M02",
+                url: "V2/{controller}/{action}/{id}",
+                defaults: new { controller = "MResearch", action = "M02", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "M01",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "MResearch", action = "M01", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
